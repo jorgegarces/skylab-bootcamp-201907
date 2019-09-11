3efe4e5d9d0d0a948021eb3b2675e374daa38361
dam0ne <dam0ne@hemwick>
2019-09-11 19:12:08 +0200

    branch cleanup

--- staff/jorge-garces/wannadog/wannadog-api/routes/chat/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    createChat: require('./create-chat'),
-    retrieveAllChats: require('./retrieve-all-chats'),
-    retrieveChat: require('./retrieve-chat'),
-    updateChat: require('./update-chat')
-}
--- staff/jorge-garces/wannadog/wannadog-api/routes/dog/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    registerDog: require('./register-dog'),
-    retrieveDog: require('./retrieve-dog'),
-    unregisterDog: require('./unregister-dog'),
-    retrieveAllDogs: require('./retrieve-all-dogs'),
-    search: require('./search')
-}
